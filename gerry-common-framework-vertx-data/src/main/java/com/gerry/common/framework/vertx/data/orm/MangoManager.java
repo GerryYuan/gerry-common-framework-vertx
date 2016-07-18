2ad5321daabb433776dc61340f5b51f21ed29f03
gerry <gerry@yunio.com>
2016-07-18 18:57:51 +0800

    添加接口备注

--- gerry-common-framework-vertx-data/src/main/java/com/gerry/common/framework/vertx/data/orm/MangoManager.java
+++ gerry-common-framework-vertx-data/src/main/java/com/gerry/common/framework/vertx/data/orm/MangoManager.java
@@ -1,5 +1,13 @@
 package com.gerry.common.framework.vertx.data.orm;
 
+/**
+ * mango操作管理接口
+ * 
+ *
+ * @author gerry
+ * @version  1.0, 2016年7月18日下午6:57:20
+ * @since   com.gerry.link 1.0
+ */
 public interface MangoManager {
 
 	static <T> T create(Class<T> clazz) {
